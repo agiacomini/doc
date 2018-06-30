3fdfeaceca35c5aad412d1ff671f0cd3104291e0
A.G <andr3a.giacomini@gmail.com>
2018-06-30 21:59:13 +0200

    update 01 script and update MySQLCommands.txt

--- sqlScript/01_DDL_PROJECT_USER.sql
+++ sqlScript/01_DDL_PROJECT_USER.sql
@@ -1,65 +1,65 @@
-REM $Id$
+-- REM $Id$
 
-/*****************************************************************
-****                                                          ****
-****                  PARTIAL COMMIT TEMPLATE                 ****
-****                                                          ****
-******************************************************************
+-- /*****************************************************************
+-- ****                                                          ****
+-- ****                  PARTIAL COMMIT TEMPLATE                 ****
+-- ****                                                          ****
+-- ******************************************************************
 
-SCRIPT NAME      : 01_DDL_PROJECT_USER.sql
+-- SCRIPT NAME      : 01_DDL_PROJECT_USER.sql
 
-AUTHOR           : 
+-- AUTHOR           : 
 
-RESPONSIBLE      : 
+-- RESPONSIBLE      :
 
-SG/ST            : 
+-- SG/ST            : 
   
-BUG/OTHER        : 
+-- BUG/OTHER        : 
   
-SYSTEM           : 
+-- SYSTEM           : 
 
-MODULE           :
+-- MODULE           :
 
-VERSION          : 1.0
+-- VERSION          : 1.0
 
-DESCRIPTION      : 
+-- DESCRIPTION      : 
 
-CONSTRAINT       :
+-- CONSTRAINT       :
 
-WARNING          :
+-- WARNING          :
 
-DATABASE         : mydatabase
+-- DATABASE         : mydatabase
 
-SCHEMA           : mydatabase
+-- SCHEMA           : mydatabase
          
-*****************************************************************
-*****************************************************************/
+-- *****************************************************************
+-- *****************************************************************/
 
-set time on
-set timing on
-set echo on
-set linesize 132
-set head off
-set scan on
-set serveroutput on 
-set define '&'
-set term on
-set feed on
+-- set time on
+-- set timing on
+-- set echo on
+-- set linesize 132
+-- set head off
+-- set scan on
+-- set serveroutput on 
+-- set define '&'
+-- set term on
+-- set feed on
 
 -- remember to change AAAnnnnn with the real script name
 
-spool 01_DDL_PROJECT_USER.log
+-- spool 01_DDL_PROJECT_USER.log
 
-SELECT global_name ||'  ' || TO_CHAR(SYSDATE, 'YYYY/MM/DD HH24:MI:SS')
-FROM global_name;
+-- SELECT global_name ||'  ' || TO_CHAR(SYSDATE, 'YYYY/MM/DD HH24:MI:SS')
+-- FROM global_name;
 
-SELECT USERNAME from user_users;
+-- SELECT USERNAME from user_users;
 
-PROMPT "ATTENTION a table will be created!!!"
-PROMPT "Press CTRL C to interrupt"
+-- PROMPT "ATTENTION a table will be created!!!"
+-- PROMPT "Press CTRL C to interrupt"
 
-SELECT '&Continue' 
-FROM dual;
+-- SELECT '&Continue' 
+-- FROM dual;
 
 CREATE TABLE project_user (
     id 		  	  INT NOT NULL AUTO_INCREMENT UNIQUE,
@@ -76,7 +76,6 @@ CREATE TABLE project_user (
     PRIMARY KEY  (id)
 );
 
-DELIMITER |
 CREATE TRIGGER PROJECT_USER_IU_TRG 
 BEFORE INSERT ON project_user
 FOR EACH ROW
@@ -92,9 +91,8 @@ FOR EACH ROW
 		SET NEW.lastUpdateBy = vUser;
 		
 	END;   
-|
 
-DESCR PROJECT_USER;
+DESCRIBE PROJECT_USER;
   
 spool off
 
